a047f7c606f8bf7feb50b200b45d798eb73d0925
SiobanL <limosanisioban@gmail.com>
2022-11-21 10:21:18 +0100

    dzldjscn,sdlc

--- TP-Algo-I/8-TP 17-11/Serie8.py
+++ TP-Algo-I/8-TP 17-11/Serie8.py
@@ -17,13 +17,35 @@ def illustration(items: list, *fs)-> None:
     for f in fs:
         temp = []
         for i in points:
-            temp.append(umons_cpu.cpu_time(f, list(reversed(range(i)))))
+            sumList = []
+            for j in range(0,2):
+                sumList.append(umons_cpu.cpu_time(f, items[j]))
+            temp.append(sum(sumList))
         grhaps.prepare(temp,f.__name__)
     grhaps.draw()
     input()
 
 
+def linear_search(lst: list, search_word: str)-> bool:
+    for w in lst:
+        if w == search_word:
+            return True
+    return False
+
+
+def dichotomic_search(t, w):
+    start = 0
+    end = len(t) - 1
+    mid = (end - start) // 2
+    while (end - start > 1) and w != t[mid]:
+        if w < t[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+            mid = start + (end - start) // 2
+    return len(t) > 0 and w == t[mid]
+
+
+
 setrecursionlimit(5000)
-illustration(list(range(50)),merge_sort,selection_sort,insertion_sort,bubble_sort)
-illustration(list(reversed(range(50))),merge_sort,selection_sort,insertion_sort,bubble_sort)
-illustration(random.shuffle(list(range(50))),merge_sort,selection_sort,insertion_sort,bubble_sort)
+illustration([list(range(50)),list(reversed(range(50))),random.shuffle(list(range(50)))],merge_sort,selection_sort,insertion_sort,bubble_sort)
